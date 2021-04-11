797d6048da9ea113e3132b93e2d6901a14caebfc
Pacco <pacco.anenin.mail@gmail.com>
2021-04-11 17:21:40 +0200

    first commit

---
+++ src/PGEngine/Game.ts
@@ -0,0 +1,69 @@
+import * as PIXI from "pixi.js";
+
+export default class Game {
+	// pixi renderer
+	protected renderer: PIXI.Renderer;
+	protected stage: PIXI.Container;
+	protected graphics: PIXI.Graphics;
+	// pixi ticker
+	protected ticker: PIXI.Ticker;
+
+	constructor(config: IGameConfig) {
+		// create renderer
+		this.renderer = new PIXI.Renderer({
+			view: config.view,
+			width: config.width,
+			height: config.height,
+			backgroundColor: config.backgroundColor,
+		});
+
+		// document.body.appendChild(this.renderer.view);
+
+		// graphics (drawings)
+
+		this.graphics = new PIXI.Graphics();
+
+		// stage
+
+		this.stage = new PIXI.Container();
+		this.stage.addChild(this.graphics);
+
+		// execute create function
+
+		this.create();
+
+		// ticker
+
+		this.ticker = new PIXI.Ticker();
+		this.ticker.add(() => {
+			// update
+			this.update(this.ticker.elapsedMS / 1000);
+			// draw
+			this.draw();
+		});
+	}
+
+	// fonction create qui s'execute une fois avant update
+	protected create(): void {}
+
+	protected update(dt: number): void {
+		// render the stage
+		this.renderer.render(this.stage);
+	}
+
+	protected draw() {
+		this.graphics.clear();
+	}
+
+	public start() {
+		this.ticker.start();
+	}
+}
+
+interface IGameConfig {
+	width: number;
+	height: number;
+	backgroundColor: number;
+	fps: number;
+	view: HTMLCanvasElement;
+}
---
+++ src/index.ts
@@ -0,0 +1,49 @@
+import Game from "./PGEngine/Game";
+import * as PIXI from "pixi.js";
+import KeyboardInputHandler from "./PGEngine/KeyboardInputHandler";
+
+export default class NewGame extends Game {
+	public static inputHandler: KeyboardInputHandler = new KeyboardInputHandler(
+		document
+	);
+
+	constructor() {
+		super({
+			view: document.getElementById("game-canvas") as HTMLCanvasElement,
+			width: 640,
+			height: 640,
+			backgroundColor: 0x4287f5,
+			fps: 60,
+		});
+	}
+
+	protected create() {
+		const helloWorld = new PIXI.Text(
+			"Hello World !",
+			new PIXI.TextStyle({ fill: ["#ffffff"] })
+		);
+		helloWorld.x = this.renderer.width / 2 - helloWorld.width / 2;
+		helloWorld.y = this.renderer.height / 2 - helloWorld.height / 2;
+
+		this.stage.addChild(helloWorld);
+	}
+
+	protected update(dt: number) {
+		super.update(dt);
+		NewGame.inputHandler.update(dt);
+	}
+
+	protected draw() {
+		super.draw();
+		this.graphics.beginFill(0);
+		this.graphics.drawCircle(
+			this.renderer.width / 2,
+			this.renderer.height / 2,
+			100
+		);
+		this.graphics.endFill();
+	}
+}
+
+const gi = new NewGame();
+gi.start();
